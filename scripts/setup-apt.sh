94968ce6f24c1129651a70f109cf057d867ed950
commander-z3r0 <cz3r0.pro@proton.me>
2026-01-23 15:10:38 +0100

    some correction on repo and mirrors

--- scripts/setup-apt.sh
+++ scripts/setup-apt.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-DEBIAN_SUITE=forky
-SUITE=$2
+MOBIAN_SUITE="trixie"   
+PARROT_SUITE="echo"        
 
-# Parrot repos (prioridad 700)
+# Parrot (suite echo)
 cat > /etc/apt/sources.list.d/parrot.list << EOF
-deb https://deb.parrot.sh/parrot ${SUITE} main contrib non-free non-free-firmware
+deb https://deb.parrot.sh/parrot ${PARROT_SUITE} main contrib non-free non-free-firmware
 EOF
 
-# Mobian repos (priority 600 - Parrot)
+# Mobian (suite trixie)
 cat > /etc/apt/sources.list.d/mobian.sources << EOF
 Types: deb
 URIs: https://repo.mobian.org/debian
-Suites: ${DEBIAN_SUITE}
+Suites: ${MOBIAN_SUITE}
 Components: main
 Signed-By: /usr/share/keyrings/mobian-archive-keyring.gpg
 EOF
@@ -23,6 +23,6 @@ Pin: origin deb.parrot.sh
 Pin-Priority: 700
 
 Package: *
-Pin: origin repo.mobian.org  
+Pin: origin repo.mobian.org
 Pin-Priority: 600
 EOF
